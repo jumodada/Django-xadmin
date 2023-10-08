f3c58c03e9eff05bbcc08a6ca183c90cef7f99a4
wukunyun <864353360@qq.com>
2023-10-08 12:28:44 +0800

    create form

--- Djangoxadmin/urls.py
+++ Djangoxadmin/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from apps.message_form.views import messgae_form
+
 urlpatterns = [
+    path('xxx/', messgae_form),
+    path('', messgae_form),
     path('admin/', admin.site.urls),
 ]
--- apps/message_form/views.py
+++ apps/message_form/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def messgae_form(request):
+    return render(request, "./form.html")
