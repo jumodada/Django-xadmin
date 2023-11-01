93157cedd18292a3f942ab340605f81046af6e11
wukunyun <864353360@qq.com>
2023-11-01 22:41:02 +0800

    update

--- apps/message_form/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def messgae_form(request):
-    return render(request, "form.html",{
-        'name': 'jay',
-        'list': ['a', 'b']
-    })
