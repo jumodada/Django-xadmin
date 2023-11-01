e2f66c5450d47353fa7eacdabbd0314b63706a0a
wukunyun <864353360@qq.com>
2023-11-01 22:48:05 +0800

    crud

--- Djangoxadmin/urls.py
+++ Djangoxadmin/urls.py
@@ -1,7 +1,7 @@
-"""Djangoxadmin URL Configuration
+"""my_project URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,9 +13,20 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', include('hello.urls')),
 ]
+handler500 = 'my_project.views.page_500'
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
--- apps/message_form/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MessageFormConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'message_form'
